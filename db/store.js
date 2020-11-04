f49fdbdd950d285cca3a41d820dfa8fdda4bff76
Cesar Martinez <cesaguma@hotmail.com>
2020-11-03 22:28:15 -0500

    Creating store.js file and adding code into apiRoute.js to call API functions from store.js

---
+++ db/store.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// LOAD DATA
+var notesData = require("../db/db.json");
+
+
+exports.getNotes = function(req,res){
+    res.json(notesData); // Display all notes
+};
+
+  // API POST Requests
+  // Creating a new note and saving into db.json file
+exports.postNotes = function(req,res){
+    let newNote = req.body;
+    let lastId;
+    if (notesData.length===0){ // Condition to control if the db.json is empty.
+      lastId=0;
+    }else {
+      lastId = notesData[notesData.length-1]["id"];
+    }
+    newNote["id"]=lastId + 1;
+    notesData.push(newNote);
+    writeFileAsync("./db/db.json", JSON.stringify(notesData)).then(function() {
+        console.log("db.json has been updated!");
+    });
+    res.json(notesData);
+};
+
+exports.deleteNotes = function(req,res){
+ //Deleting a note selected by id
+    const id = parseInt(req.params.id);
+
+    for (let i=0; i < notesData.length; i++){
+        if (id === notesData[i].id) { // Finding the note to be deleted.
+            notesData.splice(i,1);
+            let newContent = JSON.stringify(notesData,null,2); 
+            writeFileAsync("./db/db.json", newContent).then(function() {
+            console.log ("Note has been deleted!");
+            });
+        }
+    }
+    res.json(notesData);
+}; 
+
+ 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,55 +1,18 @@
 // DEPENDENCIES
-const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+const store = require("../db/store");
 
-// LOAD DATA
-
-var notesData = require("../db/db.json");
 
 // ROUTING
 
 module.exports = function(app) {
+   
   // API GET Requests
-  
-  app.get("/api/notes", function(req, res) {
-    res.json(notesData); // Display all notes
-  });
+  app.get("/api/notes", store.getNotes);
 
   // API POST Requests
-  // Creating a new note and saving into db.json file
-  app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    let lastId;
-    if (notesData.length===0){ // Condition to control if the db.json is empty.
-      lastId=0;
-    }else {
-      lastId = notesData[notesData.length-1]["id"];
-    }
-    newNote["id"]=lastId + 1;
-    notesData.push(newNote);
-    writeFileAsync("./db/db.json", JSON.stringify(notesData)).then(function() {
-        console.log("db.json has been updated!");
-    });
-    res.json(notesData);
+  app.post("/api/notes", store.postNotes);
 
-  });
-
-  //Deleting a note selected by id
-  app.delete("/api/notes/:id", function(req, res) {
-    const id = parseInt(req.params.id);
-
-    for (let i=0; i < notesData.length; i++){
-        if (id === notesData[i].id) { // Finding the note to be deleted.
-            notesData.splice(i,1);
-            let newContent = JSON.stringify(notesData,null,2); 
-            writeFileAsync("./db/db.json", newContent).then(function() {
-            console.log ("Note has been deleted!");
-            });
-        }
-    }
-    res.json(notesData);
-  });
-
+  // API DELETE Requests
+  app.delete("/api/notes/:id", store.deleteNotes);
 
 };
